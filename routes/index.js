80d364f31b60cf7dfa22e71f93ae779a83e7e3c5
kston <kenestonsc@gmail.com>
2020-08-19 17:45:22 -0300

    166 mysql functions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
+var con = require('../public/inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  con.query(`SELECT * FROM tb_menus ORDER BY title`, (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', { title: 'Restaurante Saboroso!', menus: results });
+    }
+  });
 });
 
 module.exports = router;
