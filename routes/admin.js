86045963f744957af671947ec374aadf269ccfe9
kston <kenestonsc@gmail.com>
2020-08-30 20:36:37 -0300

    175 redis config

---
+++ routes/admin.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+
+    res.render("admin/index")
+
+})
+
+router.get("/login", function(req, res, next) {
+    if(!req.session.views) req.session.views = 0;
+    console.log(req.session.views++);
+
+    res.render("admin/login")
+
+})
+
+router.get("/menus", function(req, res, next) {
+
+    res.render("admin/menus")
+
+})
+
+router.get("/emails", function(req, res, next) {
+
+    res.render("admin/emails")
+
+})
+
+router.get("/reservations", function(req, res, next) {
+
+    res.render("admin/reservations")
+
+})
+
+router.get("/contacts", function(req, res, next) {
+
+    res.render("admin/contacts")
+
+})
+
+router.get("/users", function(req, res, next) {
+
+    res.render("admin/users")
+
+})
+module.exports = router;
