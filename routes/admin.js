ddb76ac04fddc421075ced91c5001388605ec08c
kston <kenestonsc@gmail.com>
2020-08-31 09:59:50 -0300

    180 links menu and active menu

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var users = require("./../public/inc/users")
+var admin = require("./../public/inc/admin")
 var router = express.Router();
 
 
@@ -15,7 +16,12 @@ router.use(function(req, res, next){
         
     }
     
-    
+})
+
+router.use(function(req, res, next) {
+    req.menus = admin.getMenus(req);
+
+    next();
 })
 
 router.get("/logout", function(req, res, next) {
@@ -28,7 +34,9 @@ router.get("/logout", function(req, res, next) {
 router.get("/", function(req, res, next) {
   
 
-    res.render("admin/index")
+    res.render("admin/index", {
+        menus: req.menus
+    })
 
 })
 
@@ -62,13 +70,17 @@ router.post("/login", function(req, res, next) {
 
 router.get("/menus", function(req, res, next) {
 
-    res.render("admin/menus")
+    res.render("admin/menus", {
+        menus: req.menus
+    })
 
 })
 
 router.get("/emails", function(req, res, next) {
 
-    res.render("admin/emails")
+    res.render("admin/emails", {
+        menus: req.menus
+    })
 
 })
 
@@ -76,19 +88,25 @@ router.get("/reservations", function(req, res, next) {
 
     res.render("admin/reservations", {
         date: {},
+        menus: req.menus
+
     })
 
 })
 
 router.get("/contacts", function(req, res, next) {
 
-    res.render("admin/contacts")
+    res.render("admin/contacts", {
+        menus: req.menus
+    })
 
 })
 
 router.get("/users", function(req, res, next) {
 
-    res.render("admin/users")
+    res.render("admin/users", {
+        menus: req.menus
+    })
 
 })
 module.exports = router;
