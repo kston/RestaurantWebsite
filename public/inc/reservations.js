e9ae971e8fa9456c9320c612e76422fb1793cd32
kston <kenestonsc@gmail.com>
2020-09-17 15:48:42 -0300

    209

--- public/inc/reservations.js
+++ public/inc/reservations.js
@@ -1,5 +1,6 @@
 var con = require('./db');
 const Pagination = require('./Pagination');
+var moment = require('moment');
 
 module.exports = {
 	render(req, res, error, success) {
@@ -43,8 +44,10 @@ module.exports = {
 		});
 	},
 
-	save(fields) {
+	save(req) {
 		return new Promise((resolve, reject) => {
+			let fields = req.fields;
+
 			let query,
 				params = [
 					fields.name,
@@ -91,6 +94,40 @@ module.exports = {
 						reject(err);
 					} else {
 						resolve(results);
+					}
+				}
+			);
+		});
+	},
+
+	chart(req) {
+		return new Promise((resolve, reject) => {
+			con.query(
+				`
+			
+			SELECT CONCAT( YEAR(date), '-', MONTH(date)) AS date, 
+			COUNT(*) AS total, 
+			SUM(people) / COUNT(*) AS avg_people  
+			FROM tb_reservations 
+			WHERE date BETWEEN ? AND ?
+			group by YEAR(date) , MONTH(date)
+			order by YEAR(date) DESC, MONTH(date) DESC;
+			
+			
+			`,
+				[req.query.start, req.query.end],
+				(err, results) => {
+					if (err) {
+						reject(err);
+					} else {
+						let months = [];
+						let values = [];
+
+						results.forEach((row) => {
+							months.push(moment(row.date).format('MMM YYYY'));
+							values.push(row.total);
+						});
+						resolve({ months, values });
 					}
 				}
 			);
